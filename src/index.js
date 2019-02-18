49ffc6706875a06ea2bac755f31db4487ac1ad93
Colin de Roos <colinderoos@hotmail.nl>
2019-02-18 22:37:25 +0100

    remove css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
-
 export default class ExampleComponent extends Component {
   render() {
     const {
@@ -10,9 +8,7 @@ export default class ExampleComponent extends Component {
     } = this.props
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
+      <>Example Component: {text}</>
     )
   }
 }
