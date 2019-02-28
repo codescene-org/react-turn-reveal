c2cef1123b6376dd83d4da0e00c195c293fbe454
Colin de Roos <colinderoos@hotmail.nl>
2019-02-28 19:35:13 +0100

    Add initial setup for docz

---
+++ doczrc.js
@@ -0,0 +1,10 @@
+// noinspection JSUnusedGlobalSymbols
+export default {
+  title: 'React Turn Reveal',
+  description: 'A 3D reveal animation library for React.',
+  debug: true,
+  theme: 'docz-theme-default',
+  themeConfig: {
+    mode: 'dark'
+  }
+}
