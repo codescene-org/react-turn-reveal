4d0f7fb56453cd8b3b47d1dc2b053c0254f4c05a
Colin de Roos <colinderoos@gmail.com>
2019-08-03 23:41:27 +0200

    Fix duplicatad key for git add

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,10 +2,12 @@ const path = "{*,{src,docs}{/*,/**/*}}";
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  [`${path}.{js,jsx,json,mdx}`]: "eslint --fix --format friendly --max-warnings=0",
+  [`${path}.{js,jsx,json,mdx}`]: [
+    "eslint --fix --format friendly --max-warnings=0",
+    "git add"
+  ],
   [`${path}.mdx`]: "markdownlint --config .markdownlint-mdxrc",
   [`${path}.md`]: "markdownlint",
-  [`${path}.{js,jsx,json,mdx}`]: "git add",
   ".circleci/config.yml":
     'cross-env-shell test "a$CI" = a1 || circleci config validate',
   ".circleci/images/primary/Dockerfile": "dockerfile-utils lint"
