739ffa8c74eef663b83ce2b9cc136c9e9442ec49
Colin de Roos <colinderoos@gmail.com>
2019-08-03 14:21:39 +0200

    Update eslint-plugin-mdx to 0.9.1
    Resolves import errors

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,11 @@
+const path = "{*,{src,docs}{/*,/**/*}}";
+
 // eslint-disable-next-line no-undef
 module.exports = {
-  "{*,{src,docs}{/*,/**/*}}.{js,jsx,json}":
-    "eslint --fix --format friendly --max-warnings=0",
-  "{*,{src,docs}{/*,/**/*}}.mdx": [
-    "eslint --fix --format friendly --max-warnings=0",
-    "markdownlint --config .markdownlint-mdxrc"
-  ],
-  "{*,{src,docs}{/*,/**/*}}.md": "markdownlint",
-  "{*,{src,docs}{/*,/**/*}}.{js,jsx,json,mdx}": "git add",
+  [`${path}.{js,jsx,json,mdx}`]: "eslint --fix --format friendly --max-warnings=0",
+  [`${path}.mdx`]: "markdownlint --config .markdownlint-mdxrc",
+  [`${path}.md`]: "markdownlint",
+  [`${path}.{js,jsx,json,mdx}`]: "git add",
   ".circleci/config.yml":
     'cross-env-shell test "a$CI" = a1 || circleci config validate',
   ".circleci/images/primary/Dockerfile": "dockerfile-utils lint"
